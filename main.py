b71b98d1610f5ccf71380d7a54626ce1dc3ba5e8
cekz <pytrainfo@gmail.com>
2025-12-16 21:00:57 -0600

    Arreglos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------
 # ELIXIR HUB - Open Source Project
-# Copyright (C) 2025 [Tu Nombre/Organización]
+# Copyright (C) 2025 [Libba/ElixirDev]
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
